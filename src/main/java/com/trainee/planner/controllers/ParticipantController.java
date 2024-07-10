997da01859f23040b626b02a8951eb92673b8aa5
brunno <brunnolinker@gmail.com>
2024-07-09 23:45:15 -0300

    feat: create participant controller and service

---
+++ src/main/java/com/trainee/planner/controllers/ParticipantController.java
@@ -0,0 +1,36 @@
+package com.trainee.planner.controllers;
+
+import com.trainee.planner.domain.participant.Participant;
+import com.trainee.planner.dto.participant.ParticipantRequestDTO;
+import com.trainee.planner.repositories.ParticipantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/participants")
+public class ParticipantController {
+
+    @Autowired
+    private ParticipantRepository participantRepository;
+
+    @PostMapping("/{id}/confirm")
+    public ResponseEntity<Participant> confirmParticipant(@PathVariable UUID id, @RequestBody ParticipantRequestDTO payload) {
+        Optional<Participant> participant = this.participantRepository.findById(id);
+
+        if(participant.isPresent()) {
+            Participant rawParticipant = participant.get();
+            rawParticipant.setIsConfirmed(true);
+            rawParticipant.setName(payload.name());
+
+            this.participantRepository.save(rawParticipant);
+
+            return ResponseEntity.ok(rawParticipant);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
--- src/main/java/com/trainee/planner/services/participant/ParticipantService.java
+++ src/main/java/com/trainee/planner/services/participant/ParticipantService.java
@@ -1,5 +1,11 @@
 package com.trainee.planner.services.participant;
 
+import com.trainee.planner.domain.participant.Participant;
+import com.trainee.planner.domain.trip.Trip;
+import com.trainee.planner.dto.participant.ParticipantCreateResponseDTO;
+import com.trainee.planner.repositories.ParticipantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,7 +14,28 @@ import java.util.UUID;
 @Service
 public class ParticipantService {
 
-    public void registerParticipantsToEvent(List<String> participantsToInvite, UUID tripId){}
+    @Autowired
+    private ParticipantRepository participantRepository;
+
+    public void registerParticipantsToEvent(List<String> participantsToInvite, Trip trip){
+       List<Participant> participants = participantsToInvite.stream().map(email -> new Participant(email, trip)).toList();
+        this.participantRepository.saveAll(participants);
+
+        System.out.println(participants.get(0).getId());
+
+    }
+
+    public ParticipantCreateResponseDTO registerParticipantToEvent(String email, Trip trip){
+        Participant newParticipant = new Participant(email, trip);
+        this.participantRepository.save(newParticipant);
+        return new ParticipantCreateResponseDTO(newParticipant.getId());
+    }
 
     public void triggerConfirmationEmailToParticipants(UUID tripId){}
+
+    public void triggerConfirmationEmailToParticipant(String email){}
+
+    public List<Participant> getParticipants(UUID tripId) {
+        return this.participantRepository.findByTripId(tripId);
+    }
 }
